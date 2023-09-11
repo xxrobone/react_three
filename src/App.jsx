9453f4509bffea6cfa78774f905911c8f7055a2a
xxrobone <robert.wagar@edu.cmeducations.se>
2023-09-11 08:48:51 +0200

    learning three js

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import { Canvas } from '@react-three/fiber'
+import { Experience } from './components/Experience'
+
+function App() {
+
+
+  return (
+    <Canvas>
+     <Experience />
+    </Canvas>
+  )
+}
+
+export default App
